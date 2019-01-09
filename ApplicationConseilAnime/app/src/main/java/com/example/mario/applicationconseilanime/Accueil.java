f659ab122148c2c709f35e57bb219051faaae29f
scartoning2020 <35373637+scartoning2020@users.noreply.github.com>
2019-01-09 11:13:12 +0100

    requete api

--- ApplicationConseilAnime/app/src/main/java/com/example/mario/applicationconseilanime/Accueil.java
+++ ApplicationConseilAnime/app/src/main/java/com/example/mario/applicationconseilanime/Accueil.java
@@ -1,15 +1,87 @@
 package com.example.mario.applicationconseilanime;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class Accueil extends AppCompatActivity {
+
+    private String TAG = Accueil.class.getSimpleName();
+    private String countryS = "rien";
+    private TextView testJSON;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
+        testJSON = (TextView) findViewById(R.id.testJSON);
 
+        new GetContacts().execute();
     }
-}
+
+
+
+private class GetContacts extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Toast.makeText(Accueil.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        HttpHandler sh = new HttpHandler();
+        // Making a request to url and getting response
+        String url = "https://salty-ocean-70640.herokuapp.com/anime/byTitle/steins;gate";
+        String jsonStr = sh.makeServiceCall(url);
+
+        Log.e(TAG, "Response from url: " + jsonStr);
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+
+                countryS = jsonObj.getString("synopsis");
+
+
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Json parsing error: " + e.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+
+        } else {
+            Log.e(TAG, "Couldn't get json from server.");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            "Couldn't get json from server. Check LogCat for possible errors!",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        testJSON.setText(countryS);
+    }
+}}
