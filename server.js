a9d228d9f1b81d4b1282206c8405144f8560b9dc
Loowood <lorisg01@yahoo.fr>
2019-01-14 17:22:30 +0100

    Back pedal again

--- server.js
+++ server.js
@@ -17,30 +17,27 @@ const server = restify.createServer({
     version : config.version
 });
 server.use(restify.plugins.bodyParser());
-module.exports = function (ctx) {
-    const db     = ctx.db;
-    const server = ctx.server;
+const db = config.db;
 
-    // assign Users collection to variable for further use
-    const collection = db.collection('User');
+// assign Users collection to variable for further use
+const collection = db.collection('User');
 
-    server.get('/bdd', (req,res, next) => {
-        let limit = parseInt(req.query.limit, 10) || 10, // default limit to 10 docs
-            skip  = parseInt(req.query.skip, 10) || 0, // default skip to 0 docs
-            query = req.query || {};
+server.get('/bdd', (req,res, next) => {
+    let limit = parseInt(req.query.limit, 10) || 10, // default limit to 10 docs
+        skip  = parseInt(req.query.skip, 10) || 0, // default skip to 0 docs
+        query = req.query || {};
 
-        // remove skip and limit from query to avoid false querying
-        delete query.skip;
-        delete query.limit;
+    // remove skip and limit from query to avoid false querying
+    delete query.skip;
+    delete query.limit;
 
-        // find todos and convert to array (with optional query, skip and limit)
-        collection.find(query).skip(skip).limit(limit).toArray()
-            .then(docs => res.send(200, docs))
-            .catch(err => res.send(500, err));
+    // find todos and convert to array (with optional query, skip and limit)
+    collection.find(query).skip(skip).limit(limit).toArray()
+        .then(docs => res.send(200, docs))
+        .catch(err => res.send(500, err));
 
-        next()
-    });
-};
+    next()
+});
 
 server.get('/anime/byTitle/:title', function( req, res, next) {
     const promiseAnime = myanimelists.getInfoFromName(req.params.title, 'anime');
