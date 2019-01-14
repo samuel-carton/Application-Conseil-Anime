78720cdf8b3279554c2fc7f2a03da43aa50f5df4
Loowood <lorisg01@yahoo.fr>
2019-01-14 17:12:52 +0100

    Back pedal again

--- server.js
+++ server.js
@@ -70,23 +70,6 @@ server.get('/', function(req, res, err) {
     res.end();
 });
 
-server.get('/bdd', (req,res, next) => {
-    let limit = parseInt(req.query.limit, 10) || 10, // default limit to 10 docs
-        skip  = parseInt(req.query.skip, 10) || 0, // default skip to 0 docs
-        query = req.query || {};
-
-    // remove skip and limit from query to avoid false querying
-    delete query.skip;
-    delete query.limit;
-
-    // find todos and convert to array (with optional query, skip and limit)
-    collection.find(query).skip(skip).limit(limit).toArray()
-        .then(docs => res.send(200, docs))
-        .catch(err => res.send(500, err));
-
-    next()
-});
-
 server.listen(process.env.PORT || 8888, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
