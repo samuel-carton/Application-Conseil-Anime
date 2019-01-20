574b28af8b8556eeff723a7289483718ca9e5b30
scartoning2020 <35373637+scartoning2020@users.noreply.github.com>
2019-01-20 18:45:22 +0100

    search

--- ApplicationConseilAnime/app/src/main/java/com/example/mario/applicationconseilanime/Accueil.java
+++ ApplicationConseilAnime/app/src/main/java/com/example/mario/applicationconseilanime/Accueil.java
@@ -1,45 +1,72 @@
 package com.example.mario.applicationconseilanime;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.List;
 
 
-public class Accueil extends AppCompatActivity {
+public class Accueil extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = Accueil.class.getSimpleName();
-    private String countryS = "rien";
-    private TextView testJSON;
-    List<String> results;
+    private String title = "Aucun résultat";
+    private String nbEpisode = "";
+    private String genres = "";
+    private TextView TVtitle;
+    private Button BTsearch;
+    private EditText ETsearch;
+    private TextView TVnbep;
+    private TextView TVgenres;
+
+    private String search;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
-        testJSON = (TextView) findViewById(R.id.testJSON);
+        ETsearch = (EditText) findViewById(R.id.edit_text_search);
+        BTsearch = (Button) findViewById(R.id.button_search);
+        TVtitle = (TextView) findViewById(R.id.text_view_title);
+        TVnbep = (TextView) findViewById(R.id.text_view_nbep);
+        TVgenres = (TextView) findViewById(R.id.text_view_genres);
 
-       // new GetContacts().execute();
+        BTsearch.setOnClickListener(this);
 
 
-        Intent myIntent = new Intent(this, AffichageAnime.class);
-        startActivity(myIntent);
+
+
+       // Intent myIntent = new Intent(this, AffichageAnime.class);
+       // startActivity(myIntent);
 
 
     }
 
+    public void onClick(View v) {
+        search = ETsearch.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(search)){
+            new GetContacts().execute(search);
+        }
+
+    }
 
 
-private class GetContacts extends AsyncTask<Void, Void, Void> {
+private class GetContacts extends AsyncTask<String, Void, Void> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
@@ -47,10 +74,10 @@ private class GetContacts extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(Void... arg0) {
+    protected Void doInBackground(String... arg) {
         HttpHandler sh = new HttpHandler();
         // Making a request to url and getting response
-        String url = "https://salty-ocean-70640.herokuapp.com/anime/byTitle/steins;gate";
+        String url = "https://salty-ocean-70640.herokuapp.com/anime/byTitle/"+arg[0];
         String jsonStr = sh.makeServiceCall(url);
 
         Log.e(TAG, "Response from url: " + jsonStr);
@@ -58,7 +85,14 @@ private class GetContacts extends AsyncTask<Void, Void, Void> {
             try {
                 JSONObject jsonObj = new JSONObject(jsonStr);
 
-                countryS = jsonObj.getString("synopsis");
+                title = jsonObj.getString("title");
+                nbEpisode = jsonObj.getString("episodes");
+
+                JSONArray genresJSON = jsonObj.getJSONArray("genres");
+                for (int i = 0; i < genresJSON.length() && i < 3 ; i++){
+                    genres = genres+genresJSON.getString(i)+", ";
+                }
+                genres= genres.substring(0,genres.length()-2);
 
 
             } catch (final JSONException e) {
@@ -92,6 +126,9 @@ private class GetContacts extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void result) {
         super.onPostExecute(result);
-        testJSON.setText(countryS);
+        TVtitle.setText("Titre : "+Accueil.this.title);
+        TVnbep.setText("Nombre d'épisode : "+Accueil.this.nbEpisode);
+        TVgenres.setText("Genres : "+Accueil.this.genres);
+        genres = "";
     }
 }}
